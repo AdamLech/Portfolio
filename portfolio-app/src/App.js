8696f3496d4f68b044bab5851a120e18768c893f
Amogus <adammichallech@gmail.com>
2023-02-09 10:57:59 +0100

    Added home page and started animating

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {useRef} from 'react';
+import './App.css';
+
+import Home from './Home.js';
+import Contact from './Contact.js';
+import Projects from './Projects.js';
+import {createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider} from 'react-router-dom';
+
+function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path ='/contact' element={<Contact />} />
+        <Route path ='/projects' element={<Projects />} />
+      </Route>
+    )
+  )
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  );
+}
+
+const Root = () => {
+  const navBarRef = useRef();
+  const burgerRef = useRef();
+
+  const toggleNavBar = () => {
+    navBarRef.current.classList.toggle("toggle-nav-bar");
+  }
+
+  const toggleIcon = () => {
+    
+  }
+
+  return(
+    <>
+      <div className="nav-bar" ref={navBarRef}>
+          <Link to='/'>Home</Link>
+          <Link to='./Contact'>Contact</Link>
+          <Link to='./Projects'>Project</Link>
+      </div>
+      <div>
+        <div className="btn" onClick={toggleNavBar, toggleIcon}><i ref={burgerRef} className="fa-solid fa-burger"></i></div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default App;
